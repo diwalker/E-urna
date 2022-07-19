45f09dd6a33b4525d158832756e858a995675e53
Darth diX <diwalker13@icloud.com>
2022-07-19 04:04:58 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,35 @@
+let etapas = [
+
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Luis Inácio Lula da Silva',
+                partido: 'PT',
+                vice: 'Haddad',
+                fotos: [
+                    {url: '3.jpg', legenda: 'Presidente'},
+                    {url: '4.jpg', legenda: 'Vice', small: true}
+
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Jair Messias Bolsonaro',
+                partido: 'PSL',
+                vice: 'General Mourão',
+                fotos: [
+                    {url: '5.jpg', legenda: 'Presidente'},
+                    {url: '6.jpg', legenda: 'Vice', small: true}
+
+                ]
+            },
+            
+        ]
+    }
+    
+
+
+]
